75eb864c7db8a5ee1cb56b66427f8d3df99d4feb
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-08-28 16:01:07 +0100

    App import HomePageImage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Navigation from "./components/Navigation/Navigation";
+import HomePageImage from "./components/HomePageImage/HomePageImage";
 
 function App() {
   return (
     <div className="App">
       <Navigation />
+      <HomePageImage />
     </div>
   );
 }
