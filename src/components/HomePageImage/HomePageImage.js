a902f63a33119381113e1cedd79f86859c84a8b1
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-08-28 16:51:51 +0100

    HomePageImage add intro h1 and h2 text

--- src/components/HomePageImage/HomePageImage.js
+++ src/components/HomePageImage/HomePageImage.js
@@ -3,7 +3,14 @@ import React from "react";
 const HomePageImage = () => {
   return (
     <div>
-      <h2>Home Page Image</h2>
+      <header className="home-image">
+        <div className="container">
+          <div className="intro-text">
+            <h2>Hi I'm David</h2>
+            <h1>Junior Full Stack Developer</h1>
+          </div>
+        </div>
+      </header>
     </div>
   );
 };
