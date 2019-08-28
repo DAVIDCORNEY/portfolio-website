6a01467bac453c01085e668ff1991fe6c2ce9d51
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-08-28 19:43:09 +0100

    HomePageImage add projects link button

--- src/components/HomePageImage/HomePageImage.js
+++ src/components/HomePageImage/HomePageImage.js
@@ -6,8 +6,18 @@ const HomePageImage = () => {
       <header className="home-image">
         <div className="container">
           <div className="intro-text">
-            <h2>Hi I'm David</h2>
-            <h1>Junior Full Stack Developer</h1>
+            <div className="h2-intro-text">
+              <h2>Hi I'm David</h2>
+            </div>
+            <div className="h1-intro-text">
+              <h1>Junior Full Stack Developer</h1>
+            </div>
+            <a
+              class="btn btn-primary btn-xl text-uppercase js-scroll-trigger"
+              href="#services"
+            >
+              See My Projects
+            </a>
           </div>
         </div>
       </header>
