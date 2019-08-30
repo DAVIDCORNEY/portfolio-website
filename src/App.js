429228a9e97ad5d7af4720ae01fd37ea164db27e
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-08-30 11:26:04 +0100

    App import About component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Navigation from "./components/Navigation/Navigation";
 import HomePageImage from "./components/HomePageImage/HomePageImage";
 import Projects from "./components/Projects/Projects";
+import About from "./components/About/About";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Navigation />
       <HomePageImage />
       <Projects />
+      <About />
     </div>
   );
 }
