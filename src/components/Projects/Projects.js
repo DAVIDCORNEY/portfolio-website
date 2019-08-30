dab4567c9130f3e926e95c7d4d6825165ca84767
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-08-30 15:20:20 +0100

    Projects add  project images and titles

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -12,6 +12,74 @@ const Projects = () => {
             </h3>
           </div>
         </div>
+        <div className="row">
+          <div className="col-md-4 col-sm-6 project-item">
+            <a
+              class="portfolio-link"
+              data-toggle="modal"
+              href="#portfolioModal1"
+            >
+              <div class="portfolio-hover">
+                <div class="portfolio-hover-content">
+                  <i class="fas fa-plus fa-3x"></i>
+                </div>
+              </div>
+              <img
+                class="img-fluid"
+                src="img/portfolio/01-thumbnail.jpg"
+                alt=""
+              />
+            </a>
+            <div class="portfolio-caption">
+              <h4>Threads</h4>
+              <p class="text-muted">Illustration</p>
+            </div>
+          </div>
+          <div className="col-md-4 col-sm-6 project-item">
+            <a
+              class="portfolio-link"
+              data-toggle="modal"
+              href="#portfolioModal1"
+            >
+              <div class="portfolio-hover">
+                <div class="portfolio-hover-content">
+                  <i class="fas fa-plus fa-3x"></i>
+                </div>
+              </div>
+              <img
+                class="img-fluid"
+                src="img/portfolio/01-thumbnail.jpg"
+                alt=""
+              />
+            </a>
+            <div class="portfolio-caption">
+              <h4>Threads</h4>
+              <p class="text-muted">Illustration</p>
+            </div>
+          </div>
+          <div className="col-md-4 col-sm-6 project-item">
+            <a
+              class="portfolio-link"
+              data-toggle="modal"
+              href="#portfolioModal1"
+            >
+              <div class="portfolio-hover">
+                <div class="portfolio-hover-content">
+                  <i class="fas fa-plus fa-3x"></i>
+                </div>
+              </div>
+              <img
+                class="img-fluid"
+                src="img/portfolio/01-thumbnail.jpg"
+                alt=""
+              />
+            </a>
+            <div class="portfolio-caption">
+              <h4>Threads</h4>
+              <p class="text-muted">Illustration</p>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
