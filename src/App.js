1631af4fea0e212604d87d903c995a369294bc85
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-08-30 11:22:15 +0100

    App import Projects

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import Navigation from "./components/Navigation/Navigation";
 import HomePageImage from "./components/HomePageImage/HomePageImage";
+import Projects from "./components/Projects/Projects";
 
 function App() {
   return (
     <div className="App">
       <Navigation />
       <HomePageImage />
+      <Projects />
     </div>
   );
 }
