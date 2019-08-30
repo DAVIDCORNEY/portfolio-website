b8ba97a346e1470c21fa8244a79f2ec2389d1de8
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-08-30 15:21:30 +0100

    Projects add alt text to DC news thumbnail

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -26,7 +26,29 @@ const Projects = () => {
               </div>
               <img
                 class="img-fluid"
-                src="img/portfolio/01-thumbnail.jpg"
+                src="https://res.cloudinary.com/dcs/image/upload/v1567172702/Portfolio%20Website/dc-news-thumbnail-jpg.png"
+                alt="DC NEWS"
+              />
+            </a>
+            <div class="portfolio-caption">
+              <h4>Threads</h4>
+              <p class="text-muted">Illustration</p>
+            </div>
+          </div>
+          <div className="col-md-4 col-sm-6 project-item">
+            <a
+              class="portfolio-link"
+              data-toggle="modal"
+              href="#portfolioModal1"
+            >
+              <div class="portfolio-hover">
+                <div class="portfolio-hover-content">
+                  <i class="fas fa-plus fa-3x"></i>
+                </div>
+              </div>
+              <img
+                class="img-fluid"
+                src="https://res.cloudinary.com/dcs/image/upload/v1567174379/Portfolio%20Website/meet-up-app.png"
                 alt=""
               />
             </a>
@@ -48,29 +70,7 @@ const Projects = () => {
               </div>
               <img
                 class="img-fluid"
-                src="img/portfolio/01-thumbnail.jpg"
-                alt=""
-              />
-            </a>
-            <div class="portfolio-caption">
-              <h4>Threads</h4>
-              <p class="text-muted">Illustration</p>
-            </div>
-          </div>
-          <div className="col-md-4 col-sm-6 project-item">
-            <a
-              class="portfolio-link"
-              data-toggle="modal"
-              href="#portfolioModal1"
-            >
-              <div class="portfolio-hover">
-                <div class="portfolio-hover-content">
-                  <i class="fas fa-plus fa-3x"></i>
-                </div>
-              </div>
-              <img
-                class="img-fluid"
-                src="img/portfolio/01-thumbnail.jpg"
+                src="https://res.cloudinary.com/dcs/image/upload/v1567173979/Portfolio%20Website/rgb-color-picker-game.jpg.png"
                 alt=""
               />
             </a>
