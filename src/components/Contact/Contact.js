8138673ba2eb9eacd0cf6d739670473ff0e4b17f
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-08-31 12:51:39 +0100

    Contact component add basic section structure

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -2,9 +2,17 @@ import React from "react";
 
 const Contact = () => {
   return (
-    <div>
-      <h1>Contact</h1>
-    </div>
+    <section className="bg-light page-section">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-12 text-center">
+            <h2 className="section-heading text-uppercase">Contact</h2>
+            <h3 className="section-subheading text-muted">Contact Me</h3>
+          </div>
+        </div>
+        <div className="row"></div>
+      </div>
+    </section>
   );
 };
 
