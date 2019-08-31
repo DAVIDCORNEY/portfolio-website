428ca893f13e57a0640a637729d6855b81c5d2b2
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-08-31 16:21:12 +0100

    About component add intro text

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -8,19 +8,32 @@ const About = () => {
         <div className="row">
           <div className="col-lg-12 text-center">
             <h2 className="section-heading text-uppercase">About</h2>
-            <h3 className="section-subheading text-muted">About Me</h3>
           </div>
         </div>
-        <div className="row">
+        <div className="row mt-4">
           <div className="col-sm-5">
             <div className="about-me">
               <img
-                class="mx-auto rounded-circle"
+                className="mx-auto rounded-circle"
                 src="https://res.cloudinary.com/dcs/image/upload/v1567261524/Portfolio%20Website/david-corney.jpg"
                 alt="David Corney"
               />
               <h4>David Corney</h4>
-              <p class="text-muted">Junior Developer</p>
+              <p className="text-muted">Junior Developer</p>
+            </div>
+          </div>
+          <div className="col-sm-7">
+            <div className="intro-text">
+              <p>
+                I am a Junior Full Stack Developer recently graduated from the
+                Northcoders coding Bootcamp in Manchester. Track record of
+                working well in a team in a fast-paced environment and working
+                well under my initiative. Over 10 years of experience in a
+                customer-facing retail environment and further management and
+                technical sales experience in the Plastic Extrusion Industry.
+                Experience in listening to and evaluating customer requirements
+                and training new employees.
+              </p>
             </div>
           </div>
         </div>
