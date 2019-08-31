1fee29e68830815ff2bad7bc5224043e294a69d4
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-08-31 12:47:52 +0100

    About component add basic section structure

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,9 +2,17 @@ import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
-    </div>
+    <section className="page-section">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-12 text-center">
+            <h2 className="section-heading text-uppercase">About</h2>
+            <h3 className="section-subheading text-muted">About Me</h3>
+          </div>
+        </div>
+        <div className="row"></div>
+      </div>
+    </section>
   );
 };
 
