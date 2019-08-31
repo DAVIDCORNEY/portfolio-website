8fe23c61b927dbff5e91b4a073deef3873c97f9f
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-08-31 13:22:24 +0100

    ProjectCard add project-link classes

--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -3,15 +3,15 @@ import React from "react";
 const ProjectCard = ({ thumbNail, alt, projectTitle, description }) => {
   return (
     <div className="col-md-4 col-sm-6 project-item">
-      <a className="portfolio-link" data-toggle="modal" href="#portfolioModal1">
-        <div className="portfolio-hover">
-          <div className="portfolio-hover-content">
+      <a className="project-link" data-toggle="modal" href="#projectModal1">
+        <div className="project-hover">
+          <div className="project-hover-content">
             <i className="fas fa-plus fa-3x"></i>
           </div>
         </div>
         <img className="img-fluid" src={thumbNail} alt={alt} />
       </a>
-      <div className="portfolio-caption">
+      <div className="project-caption">
         <h4>{projectTitle}</h4>
         <p className="text-muted">{description}</p>
       </div>
