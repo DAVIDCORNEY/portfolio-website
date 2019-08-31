4b6df1fd1a5b9d99a2a85bf9fe31993d8f947cc9
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-08-31 14:29:31 +0100

    About.js add id=about to section

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const About = () => {
   return (
-    <section className="page-section">
+    <section className="page-section" id="about">
       <div className="container">
         <div className="row">
           <div className="col-lg-12 text-center">
