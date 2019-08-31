8b0e7e86909da99d1a5e29857dad496960a68563
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-08-31 11:25:27 +0100

    Add reuseable ProjectCard's into Projects component

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -14,51 +14,30 @@ const Projects = () => {
           </div>
         </div>
         <div className="row">
-          <ProjectCard />
-          <div className="col-md-4 col-sm-6 project-item">
-            <a
-              class="portfolio-link"
-              data-toggle="modal"
-              href="#portfolioModal1"
-            >
-              <div class="portfolio-hover">
-                <div class="portfolio-hover-content">
-                  <i class="fas fa-plus fa-3x"></i>
-                </div>
-              </div>
-              <img
-                class="img-fluid"
-                src="https://res.cloudinary.com/dcs/image/upload/v1567174379/Portfolio%20Website/meet-up-app.png"
-                alt=""
-              />
-            </a>
-            <div class="portfolio-caption">
-              <h4>Threads</h4>
-              <p class="text-muted">Illustration</p>
-            </div>
-          </div>
-          <div className="col-md-4 col-sm-6 project-item">
-            <a
-              class="portfolio-link"
-              data-toggle="modal"
-              href="#portfolioModal1"
-            >
-              <div class="portfolio-hover">
-                <div class="portfolio-hover-content">
-                  <i class="fas fa-plus fa-3x"></i>
-                </div>
-              </div>
-              <img
-                class="img-fluid"
-                src="https://res.cloudinary.com/dcs/image/upload/v1567173979/Portfolio%20Website/rgb-color-picker-game.jpg.png"
-                alt=""
-              />
-            </a>
-            <div class="portfolio-caption">
-              <h4>Threads</h4>
-              <p class="text-muted">Illustration</p>
-            </div>
-          </div>
+          <ProjectCard
+            thumbNail={
+              "https://res.cloudinary.com/dcs/image/upload/v1567172702/Portfolio%20Website/dc-news-thumbnail-jpg.png"
+            }
+            alt={"DC News"}
+            projectTitle={"Threads"}
+            description={"Illustration"}
+          />
+          <ProjectCard
+            thumbNail={
+              "https://res.cloudinary.com/dcs/image/upload/v1567174379/Portfolio%20Website/meet-up-app.png"
+            }
+            alt={"MeetUp"}
+            projectTitle={"MeetUp"}
+            description={"Meeting App"}
+          />
+          <ProjectCard
+            thumbNail={
+              "https://res.cloudinary.com/dcs/image/upload/v1567173979/Portfolio%20Website/rgb-color-picker-game.jpg.png"
+            }
+            alt={"RGB Colour Picker Game"}
+            projectTitle={"Colour Picker"}
+            description={"Colour Picker Game"}
+          />
         </div>
       </div>
     </section>
