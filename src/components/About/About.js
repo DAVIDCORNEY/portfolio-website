81629cabd722d28346f42bc7d2693a4830891eff
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-08-31 16:52:23 +0100

    About component rearrange layout to one column

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -11,7 +11,7 @@ const About = () => {
           </div>
         </div>
         <div className="row mt-4">
-          <div className="col-sm-5">
+          <div className="col-lg-12">
             <div className="about-me">
               <img
                 className="mx-auto rounded-circle"
@@ -20,20 +20,60 @@ const About = () => {
               />
               <h4>David Corney</h4>
               <p className="text-muted">Junior Developer</p>
-            </div>
-          </div>
-          <div className="col-sm-7">
-            <div className="intro-text">
               <p>
                 I am a Junior Full Stack Developer recently graduated from the
                 Northcoders coding Bootcamp in Manchester. Track record of
                 working well in a team in a fast-paced environment and working
-                well under my initiative. Over 10 years of experience in a
-                customer-facing retail environment and further management and
-                technical sales experience in the Plastic Extrusion Industry.
-                Experience in listening to and evaluating customer requirements
-                and training new employees.
+                well under my initiative.I am a Junior Full Stack Developer
+                recently graduated from the Northcoders coding Bootcamp in
+                Manchester. Track record of working well in a team in a
+                fast-paced environment and working well under my initiative.
               </p>
+            </div>
+          </div>
+        </div>
+        <div className="col-lg-12">
+          <div className="skills">
+            <h4 className="text-muted">Programming Languages and Tools</h4>
+            <div className="skills-icons">
+              <ul class="list-inline dev-icons">
+                <li class="list-inline-item">
+                  <i class="fab fa-html5"></i>
+                </li>
+                <li class="list-inline-item">
+                  <i class="fab fa-css3-alt"></i>
+                </li>
+                <li class="list-inline-item">
+                  <i class="fab fa-js-square"></i>
+                </li>
+                <li class="list-inline-item">
+                  <i class="fab fa-angular"></i>
+                </li>
+                <li class="list-inline-item">
+                  <i class="fab fa-react"></i>
+                </li>
+                <li class="list-inline-item">
+                  <i class="fab fa-node-js"></i>
+                </li>
+                <li class="list-inline-item">
+                  <i class="fab fa-sass"></i>
+                </li>
+                <li class="list-inline-item">
+                  <i class="fab fa-less"></i>
+                </li>
+                <li class="list-inline-item">
+                  <i class="fab fa-wordpress"></i>
+                </li>
+                <li class="list-inline-item">
+                  <i class="fab fa-gulp"></i>
+                </li>
+                <li class="list-inline-item">
+                  <i class="fab fa-grunt"></i>
+                </li>
+                <li class="list-inline-item">
+                  <i class="fab fa-npm"></i>
+                </li>
+              </ul>
             </div>
           </div>
         </div>
