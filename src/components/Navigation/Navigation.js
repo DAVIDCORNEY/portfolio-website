00bc693834bd2c66abd65cacb27a99b36655453e
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-08-31 14:30:28 +0100

    Navigation add about id to href in about link

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -30,7 +30,7 @@ const Navigation = () => {
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">
+              <a className="nav-link" href="#about">
                 About
               </a>
             </li>
