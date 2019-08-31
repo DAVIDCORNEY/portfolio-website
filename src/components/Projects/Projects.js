413aeca6d9bb81f03280dfcf9c9cf57e52728d8e
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-08-31 11:03:21 +0100

    Projects component remove 1st project card

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -26,28 +26,6 @@ const Projects = () => {
               </div>
               <img
                 class="img-fluid"
-                src="https://res.cloudinary.com/dcs/image/upload/v1567172702/Portfolio%20Website/dc-news-thumbnail-jpg.png"
-                alt="DC NEWS"
-              />
-            </a>
-            <div class="portfolio-caption">
-              <h4>Threads</h4>
-              <p class="text-muted">Illustration</p>
-            </div>
-          </div>
-          <div className="col-md-4 col-sm-6 project-item">
-            <a
-              class="portfolio-link"
-              data-toggle="modal"
-              href="#portfolioModal1"
-            >
-              <div class="portfolio-hover">
-                <div class="portfolio-hover-content">
-                  <i class="fas fa-plus fa-3x"></i>
-                </div>
-              </div>
-              <img
-                class="img-fluid"
                 src="https://res.cloudinary.com/dcs/image/upload/v1567174379/Portfolio%20Website/meet-up-app.png"
                 alt=""
               />
