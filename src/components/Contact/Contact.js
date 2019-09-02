242ccf22c868493de274a5d73133243f85ea4e72
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-09-02 15:08:15 +0100

    Contact component split into 3 columns set button to large

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -12,8 +12,8 @@ const Contact = () => {
   return (
     <section className="bg-light page-section" id="contact">
       <div className="container">
-        <div className="row">
-          <div className="col-lg-12 text-center">
+        <div className="row justify-content-center">
+          <div className="col-lg-8 text-center">
             <h2 className="section-heading text-uppercase">Contact</h2>
             <h3 className="section-subheading text-muted">
               To get in touch, please feel free to email, call or send a message
@@ -22,21 +22,23 @@ const Contact = () => {
           </div>
         </div>
         <div className="row">
-          <div className="col-lg-6 text-left contact-details">
-            <p>
-              <FaMapMarkerAlt /> Bradford, UK
-            </p>
-            <p>
-              <FaMobileAlt /> <a href="tel:07525 814776">07525 814776</a>
-            </p>
-            <p>
-              <FaEnvelope />{" "}
-              <a href="mailto:cdavidmill@aol.com">cdavidmill@aol.com</a>
-            </p>
+          <div className="col-lg-4 contact-details">
+            <div className="contact-details-list">
+              <p>
+                <FaMapMarkerAlt /> Bradford, UK
+              </p>
+              <p>
+                <FaMobileAlt /> <a href="tel:07525 814776">07525 814776</a>
+              </p>
+              <p>
+                <FaEnvelope />
+                <a href="mailto:cdavidmill@aol.com"> cdavidmill@aol.com</a>
+              </p>
+            </div>
           </div>
-          <div className="col-lg-6 social-links">
-            <ul class="list-inline social-buttons">
-              <li class="list-inline-item">
+          <div className="col-lg-4 social-links">
+            <ul className="list-inline social-buttons">
+              <li className="list-inline-item">
                 <a
                   href="https://github.com/DAVIDCORNEY"
                   target="_blank"
@@ -45,7 +47,7 @@ const Contact = () => {
                   <FaGithub />
                 </a>
               </li>
-              <li class="list-inline-item">
+              <li className="list-inline-item">
                 <a
                   href="https://www.linkedin.com/in/david-corney/"
                   target="_blank"
@@ -55,8 +57,10 @@ const Contact = () => {
                 </a>
               </li>
             </ul>
+          </div>
+          <div className="col-lg-4">
             <a
-              className="btn btn-primary download-cv"
+              className="btn btn-primary btn-lg download-cv"
               href="https://res.cloudinary.com/dcs/image/upload/v1567420059/David%20Corney%20CV/David-Corney-CV.pdf"
               target="_blank"
               rel="noopener noreferrer"
