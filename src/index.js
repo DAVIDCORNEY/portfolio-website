ee8e89966b60b442e05b326b01a298988ec5aa49
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-09-02 16:32:22 +0100

    Index.js import popper.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "popper.js/dist/popper.min.js";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
