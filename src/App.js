7da5e5dce0448ee206149b2dae6d2e6cabd9d589
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-09-07 19:15:55 +0100

    App.js set up for routing to home

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import React from "react";
-import { Router, navigate } from "@reach/router";
+import { Router } from "@reach/router";
+import Home from "./components/Home/Home";
 import "./App.css";
-import Navigation from "./components/Navigation/Navigation";
-import HomePageImage from "./components/HomePageImage/HomePageImage";
-import Projects from "./components/Projects/Projects";
-import About from "./components/About/About";
-import Contact from "./components/Contact/Contact";
-import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <Navigation />
-      <HomePageImage />
-      <Projects />
-      <About />
-      <Contact />
-      <Footer />
-    </div>
+    <Router>
+      <Home path="/" />
+    </Router>
   );
 }
 
