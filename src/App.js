76ea341ece016332904a5eda91ec06c52740665b
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-09-08 18:33:59 +0100

    ProjectModal change class to className

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import "./App.css";
 function App() {
   return (
     <Router>
-      <Home path="/portfolio-website/" />
+      <Home path="/" />
     </Router>
   );
 }
--- src/components/ProjectModal/ProjectModal.js
+++ src/components/ProjectModal/ProjectModal.js
@@ -16,27 +16,27 @@ const ProjectModal = ({
 }) => {
   return (
     <div
-      class="project-modal modal fade"
+      className="project-modal modal fade"
       id={id}
-      tabindex="-1"
+      tabIndex="-1"
       role="dialog"
       aria-hidden="true"
     >
-      <div class="modal-dialog">
-        <div class="modal-content">
-          <div class="close-modal" data-dismiss="modal">
-            <div class="lr">
-              <div class="rl"></div>
+      <div className="modal-dialog">
+        <div className="modal-content">
+          <div className="close-modal" data-dismiss="modal">
+            <div className="lr">
+              <div className="rl"></div>
             </div>
           </div>
-          <div class="container">
-            <div class="row">
-              <div class="col-lg-8 mx-auto">
-                <div class="modal-body">
-                  <h2 class="text-uppercase">{projectTitle}</h2>
-                  <p class="item-intro text-muted">{description}</p>
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-8 mx-auto">
+                <div className="modal-body">
+                  <h2 className="text-uppercase">{projectTitle}</h2>
+                  <p className="item-intro text-muted">{description}</p>
                   <img
-                    class="img-fluid d-block mx-auto"
+                    className="img-fluid d-block mx-auto"
                     src={thumbNail}
                     alt=""
                   />
@@ -53,11 +53,11 @@ const ProjectModal = ({
                     <li>{backEndCode}</li>
                   </ul>
                   <button
-                    class="btn btn-primary"
+                    className="btn btn-primary"
                     data-dismiss="modal"
                     type="button"
                   >
-                    <i class="fas fa-times"></i>
+                    <i className="fas fa-times"></i>
                     Close Project
                   </button>
                 </div>
